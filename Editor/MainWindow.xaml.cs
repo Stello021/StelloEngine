2879192125f81e82857addf4c96e54cd69eae0cd
Stello021 <stellidan@gmail.com>
2025-09-03 17:16:51 +0200

    Loaded Project Browser Dialog
    -Attached Project Browser Dialog to Main Window (loadable once time only)
    - When Project Browser Dialog is closed, shutdown the application (see OpenProjectBrowserDialog() implementantion)

--- Editor/MainWindow.xaml.cs
+++ Editor/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Editor.GameProject;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,25 @@ namespace Editor
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += OnMainWindowLoaded;
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            //Recursive Detaching to allows main window loading once only
+            Loaded -= OnMainWindowLoaded;
+            OpenProjectBrowserDialog();
+        }
+
+        private void OpenProjectBrowserDialog()
+        {
+            var projectBrowser = new ProjectBrowserDialog();
+            if (projectBrowser.ShowDialog() == false) //When dialog is closed with Cancel or closed button
+            {
+                // Close the main window and shutdown the application
+                Application.Current.Shutdown();
+                return;
+            }
         }
     }
 }
